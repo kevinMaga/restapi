4080f190c1cf74b54419a42017f781a7f383ca5d
kevinMaga <kerimaga@espol.edu.ec>
2024-07-15 09:52:52 -0500

    Guia17

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
+
+/* Referencia al módulo swagger-ui-express */
+const swaggerUi = require('swagger-ui-express')
+
+/* Referencia al archivo con la descripción */
+const swaggerFile = require('./swagger_output.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+/* Ruta Base -> Documentación */
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use('/api', require('./routes/api'));
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api', require('./routes/api'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
